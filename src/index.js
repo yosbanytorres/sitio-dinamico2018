c2889138e1d73f7c13362e5d50d35099ad81e4db
Yosbany <yosbanytorres@gmail.com>
2018-11-18 07:53:12 +0100

    cambio

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+import './js/config'
+import './scss/main.scss'
+import tplHome from './html/home.tpl.html'
+import tplAbout from './html/about.tpl.html'
+import tplContact from './html/contact.tpl.html'
+import tplAdmin from './html/admin.tpl.html'
+import tplAdminAuth from './html/admin_auth.tpl.html'
+
